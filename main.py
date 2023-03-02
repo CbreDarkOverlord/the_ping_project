2a2965d907ad56e02f302944afe3c8ec30db92ec
Dark Overlord <dark.overlord@cbre.com>
2023-03-01 20:28:59 -0700

    refactored for speed

--- main.py
+++ main.py
@@ -6,19 +6,21 @@ workbook = openpyxl.load_workbook('example.xlsx')
 sheet = workbook['Sheet1']
 
 # Loop through each row in the sheet, and ping each server in turn
-values = []
-for row in sheet.iter_rows(min_row=1, values_only=True):
-    values.append(row[0])
+values = [row[0] for row in sheet.iter_rows(min_row=1, values_only=True)]
 
-for i in range(len(values)):
-    server_name = values[i]
+# Loop through each server and ping it once
+rows = []
+for i, server_name in enumerate(values):
     print(f'{server_name}')
-    if ping(server_name) is None or ping(server_name) is False:
-        print(f'{server_name} is down')
-        sheet.cell(row=i+1, column=2).value = 'Down'
-    else:
-        print(f'{server_name} responded in {ping(server_name)} ms')
-        sheet.cell(row=i+1, column=2).value = 'Up'
+    response_time = ping(server_name)
+    status = 'Down' if response_time is None or response_time is False else 'Up'
+    rows.append((server_name, status, response_time))
+    print(f'{server_name} is {status}')
+
+# Write the results to the worksheet using batch cell writing
+sheet.append(['Server Name', 'Status', 'Response Time (ms)'])
+for row in rows:
+    sheet.append(row)
 
 # save all the hawtness to the spreadsheet and crack a cold one mate, yer job's done
 workbook.save("example.xlsx")
