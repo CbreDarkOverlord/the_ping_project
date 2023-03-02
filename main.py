7973a45483ecf1ff5fb487b22f20fcd258ad32d8
Dark Overlord <dark.overlord@cbre.com>
2023-03-01 21:40:39 -0700

    updating to proper formatting

--- main.py
+++ main.py
@@ -2,25 +2,23 @@ import openpyxl
 from ping3 import ping
 
 # Open the Excel file and select the sheet containing the server names
-workbook = openpyxl.load_workbook('example.xlsx')
+workbook = openpyxl.load_workbook('no access to server - Jan-2023.xlsx')
 sheet = workbook['Sheet1']
 
 # Loop through each row in the sheet, and ping each server in turn
 values = [row[0] for row in sheet.iter_rows(min_row=1, values_only=True)]
 
 # Loop through each server and ping it once
-rows = []
 for i, server_name in enumerate(values):
     print(f'{server_name}')
     response_time = ping(server_name)
-    status = 'Down' if response_time is None or response_time is False else 'Up'
-    rows.append((server_name, status, response_time))
-    print(f'{server_name} is {status}')
-
-# Write the results to the worksheet using batch cell writing
-sheet.append(['Server Name', 'Status', 'Response Time (ms)'])
-for row in rows:
-    sheet.append(row)
+    if response_time is None or response_time is False:
+        print(f'{server_name} is down')
+        sheet.cell(row=i+1, column=2).value = 'Down'
+    else:
+        print(f'{server_name} responded in {response_time} ms')
+        sheet.cell(row=i+1, column=2).value = 'Up'
+        sheet.cell(row=i+1, column=3).value = response_time
 
 # save all the hawtness to the spreadsheet and crack a cold one mate, yer job's done
-workbook.save("example.xlsx")
+workbook.save('no access to server - Jan-2023.xlsx')
